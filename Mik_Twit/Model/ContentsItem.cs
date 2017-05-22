e1f78de6695154f5e9fefc9450d775c95f4a34ce
mik-claire <mekakushi.mkz@gmail.com>
2017-05-23 00:58:02 +0900

    Fixed typo.

--- Mik_Twit/Model/ContentsItem.cs
+++ Mik_Twit/Model/ContentsItem.cs
@@ -6,7 +6,7 @@
 
         public ContentsItem(string contentsUrl)
         {
-            if (!contentsUrl.EndWith(":orig"))
+            if (!contentsUrl.EndsWith(":orig"))
             {
                 contentsUrl += ":orig";
             }
